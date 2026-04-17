8fc3ed10fafd9a69a7a55669b7beff254cf59f10
aiai1992 <45343783+aiai1992@users.noreply.github.com>
2026-04-17 18:36:26 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './english_rewriter_mini_tool (1).jsx'
+import App from './App.jsx' // 名前をApp.jsxに変えたので、ここも修正
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
